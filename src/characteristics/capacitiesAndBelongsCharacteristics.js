5507292d6fd2865b6653f85990a039ce5980e5ec
Parrish <paarriisshh@yahoo.com.mx>
2019-10-23 19:46:16 -0500

    add capacitiesAndBelongsCharacteristics

---
+++ src/characteristics/capacitiesAndBelongsCharacteristics.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    category: 'physical',
+    belongs: ['strength', 'dexterity', 'agility', 'physique']
+  },
+  {
+    category: 'psychic',
+    belongs: ['inteligence', 'power', 'will', 'perception']
+  }
+]
