71c41749adc8f87e9941e7e56bfe6c3a94cd735d
Parrish <paarriisshh@yahoo.com.mx>
2019-11-03 00:14:23 -0600

    add arrayValueComparators
    this is a module for match in a array the value in a index, in the future all matchers with a array and a index must be here, secondary characteristics height weight and size check uses
    todo: test modules

---
+++ src/utils/arrayValueComparators.js
@@ -0,0 +1,12 @@
+const compareWihtIndex = require('./compareWithIndex')
+
+const isLessOrEqual = (array, index, value) => compareWihtIndex(array, index, value, (indexValue, value) => value <= indexValue)
+
+const isGreaterOrEqual = (array, index, value) => compareWihtIndex(
+  array, index, value,
+  (indexValue, value) => value >= indexValue)
+
+module.exports = {
+  isLessOrEqual,
+  isGreaterOrEqual
+}
---
+++ src/utils/compareWithIndex.js
@@ -0,0 +1,4 @@
+module.exports = (array, index, value, fnComparator) => {
+  const indexValue = array[index]
+  return fnComparator(indexValue, value, index, array)
+}
