e281156dee129abaff50f52669c7b1403ece3831
Parrish <paarriisshh@yahoo.com.mx>
2019-11-20 20:35:53 -0600

    Physiscal capacities is now NamedValue based

--- src/physicalCapacities/physicalCapacities.js
+++ src/physicalCapacities/physicalCapacities.js
@@ -1,16 +1,9 @@
-class PhysicalCapacities {
-  constructor (agility, physique) {
-    this._agility = agility
-    this._physique = physique
+const NamedValueCollection = require('../NamedValue/NamedValueColection')
+const PhysicalCapacity = require('./PhysicalCapacity')
+class PhysicalCapacities extends NamedValueCollection {
+  constructor (names, values) {
+    super(names, values, PhysicalCapacity)
   }
-
-  get agility () { return this._agility }
-
-  set agility (val) { throw new Error('Agility is read only') }
-
-  get physique () { return this._physique }
-
-  set physique (val) { throw new Error('physique is read only') }
 }
 
 module.exports = PhysicalCapacities
