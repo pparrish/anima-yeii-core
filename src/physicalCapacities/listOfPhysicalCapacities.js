34a5ccf339a2b72cf4e6e2f7ed57b65e8429a412
Parrish <paarriisshh@yahoo.com.mx>
2019-11-20 18:49:46 -0600

    add listOfPhysicalCapacities

---
+++ src/physicalCapacities/listOfPhysicalCapacities.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    name: 'fatigue',
+    type: 'int',
+    linkedTo: 'characteristics/strength'
+  },
+  {
+    name: 'movenent type',
+    type: 'int',
+    linkedTo: 'characteristics/agility'
+  }
+]
