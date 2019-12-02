a558ea424b3eadd41a267d05da98bedfc2002788
Parrish <paarriisshh@yahoo.com.mx>
2019-12-02 12:51:41 -0600

    the list of combat habilities now is a list of abilities"

--- src/primaryAbilities/combatAbilities/listOfCombatAbilities.js
+++ src/primaryAbilities/combatAbilities/listOfCombatAbilities.js
@@ -1,9 +1,26 @@
-module.exports = [
-  'atack',
-  'stop',
-  'doge',
-  'ki',
-  'weapon table',
-  'martial arts',
-  'wear armor'
+const createList = require('../../utils/createList')
+const CombatAbility = require('./CombatAbility')
+const list = [
+  {
+    name: 'atack',
+    dependency: 'dexterity',
+    CREATOR: CombatAbility
+  },
+  {
+    name: 'stop',
+    dependency: 'dexterity',
+    CREATOR: CombatAbility
+  },
+  {
+    name: 'dodge',
+    dependency: 'agility',
+    CREATOR: CombatAbility
+  },
+  {
+    name: 'wear armor',
+    dependency: 'strength',
+    CREATOR: CombatAbility
+  }
 ]
+
+module.exports = createList(list)
--- src/primaryAbilities/combatAbilities/listOfTypesOfCombatAbilities.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  atack: 'int',
-  stop: 'int',
-  doge: 'int',
-  ki: 'int',
-  'weapon table': 'table',
-  'wear armor': 'int'
-}
