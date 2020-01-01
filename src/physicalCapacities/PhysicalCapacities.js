10f6af22de113a2162598ce9b77e6d33f6cc9a6c
Parrish <paarriisshh@yahoo.com.mx>
2020-01-01 16:37:09 -0600

    add PhysicalCapacities#set

--- src/physicalCapacities/PhysicalCapacities.js
+++ src/physicalCapacities/PhysicalCapacities.js
@@ -1,8 +1,15 @@
+const { required } = require('../utils').classUtils
 const NamedValueCollection = require('../NamedValue/NamedValueColection')
 const listOfPhysicalCapacities = require('./listOfPhysicalCapacities')
 class PhysicalCapacities extends NamedValueCollection {
   constructor () {
     super(listOfPhysicalCapacities)
+  }
+
+  set (name, value = required('value')) {
+    const physicalCapacity = this.get(name)
+    this._.storage.set(name, physicalCapacity.fromOptions({ name, value }))
+    return this
   }
 }
 
