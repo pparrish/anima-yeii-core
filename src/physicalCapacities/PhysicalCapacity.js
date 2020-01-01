e82edfeebed41a229211fd6c0ca3cada19017c43
Parrish <paarriisshh@yahoo.com.mx>
2020-01-01 15:40:41 -0600

    add PhysicalCapacity#fromOptions

--- src/physicalCapacities/PhysicalCapacity.js
+++ src/physicalCapacities/PhysicalCapacity.js
@@ -1,6 +1,14 @@
 const NamedValue = require('../NamedValue/NamedValue')
 
 class PhysicalCapacity extends NamedValue {
+  static fromOptions (options) {
+    const { name, value } = options
+    return PhysicalCapacity(name, value)
+  }
+
+  fromOptions (options) {
+    return PhysicalCapacity.fromOptions(options)
+  }
 }
 
 module.exports = PhysicalCapacity
