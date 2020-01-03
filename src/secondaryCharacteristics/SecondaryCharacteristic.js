597a1871a6dba7deea769a717eae7d08813bd740
Parrish <paarriisshh@yahoo.com.mx>
2020-01-02 20:56:16 -0600

    add SecondaryCharacteristic#fromOptions

--- src/secondaryCharacteristics/SecondaryCharacteristic.js
+++ src/secondaryCharacteristics/SecondaryCharacteristic.js
@@ -1,6 +1,14 @@
 const NamedValue = require('../NamedValue/NamedValue')
 
 class SecondaryCharacteristic extends NamedValue {
+  static fromOptions (options) {
+    const { name, value } = options
+    return new SecondaryCharacteristic(name, value)
+  }
+
+  fromOptions (options) {
+    return SecondaryCharacteristic.fromOptions(options)
+  }
 }
 
 module.exports = SecondaryCharacteristic
